f3e1c6f71f9bb02ebeb72ac996268a9ecad8b6dd
Kontae <91358761+Kontae@users.noreply.github.com>
2024-02-07 15:51:56 +0900

    [Silver IV] Title: 로프, Time: 20 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int n, x, cnt, ans = -1;
+int MAX(int a, int b)
+{
+	return (a > b ? a : b);
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n;
+	priority_queue<int> pq;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		pq.push(x);
+	}
+	while (!pq.empty())
+	{
+		cnt++;
+		int num = pq.top();
+		if (ans < num * cnt) ans = num * cnt;
+		pq.pop();
+	}
+	cout << ans << '\n';
+	return 0;
+}
