cf3a4eeb1d2f98ba2b78aee5ad0ec91c5f2f75bd
Kontae <91358761+Kontae@users.noreply.github.com>
2024-02-27 16:14:13 +0900

    [level 2] Title: 오픈채팅방, Time: 203.00 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    vector<string> v;
+    map<string,string> m;
+    vector<string> result;
+    for(int i=0;i<record.size();i++)
+    {
+        string fi,se,th;
+        stringstream ss;
+        ss.str(record[i]);
+        ss>>fi>>se>>th;
+        
+        if(fi=="Enter")
+        {
+            v.push_back("님이 들어왔습니다.");
+            answer.push_back(se);
+            m[se]=th;
+        }
+        else if(fi=="Leave")
+        {
+            v.push_back("님이 나갔습니다.");
+            answer.push_back(se);
+        }
+        else if(fi=="Change")
+        {
+            m[se]=th;
+        }
+    }
+    for(int i=0;i<answer.size();i++)
+    {
+        result.push_back(m[answer[i]]+v[i]);
+    }
+    return result;
+}
