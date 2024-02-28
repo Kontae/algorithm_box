9654e2091f251835b7d64b335d2a9feedb162383
Kontae <91358761+Kontae@users.noreply.github.com>
2024-02-28 17:51:14 +0900

    [level 2] Title: 요격 시스템, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> targets) {
+    int answer = 1;
+    sort(targets.begin(),targets.end());
+    int end = targets[0][1];
+    for(int i=1;i<targets.size();i++)
+    {
+        if(targets[i][0]<end)
+        {
+            end=min(targets[i][1],end);
+        }
+        else
+        {
+            answer++;
+            end=targets[i][1];
+        }
+    }
+    return answer;
+}
