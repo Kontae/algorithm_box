31cb7d11783149597b4df84e11c3749a26461f6f
Kontae <91358761+Kontae@users.noreply.github.com>
2025-05-10 17:44:44 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map<string,int> m;
+    for(auto &x: clothes)   m[x[1]]++;
+    for(auto it=m.begin();it!=m.end();it++) answer*=((*it).second+1);
+    return answer-1;
+}
