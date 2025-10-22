52971298c075fb43de2946de3d0035ef99f55172
Kontae <91358761+Kontae@users.noreply.github.com>
2025-10-22 19:12:30 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        result = new int[M];
+
+        Comb(0, 1);
+
+        System.out.println(sb);
+    }
+
+    private static void Comb(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            result[depth] = i;
+            Comb(depth + 1, i + 1);
+        }
+    }
+}
